784e4adabcbbbe8827f45e80d622b9230fead6c9
user.name like:"2002amankr3121@gmail.com <2002amankr3121@gmail.com>
2026-01-07 23:56:58 +0530

    full backend complete

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,19 @@
 const express = require ('express');
+const cors = require('cors');
+const aiRoutes = require("./routes/ai.routes")
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json())
 
 app.get('/',(req,res) =>{
   res.send('hello world')
 })
+
+
+
+app.use('/ai', aiRoutes)
 
 module.exports = app
--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -1,8 +1,8 @@
 const express = require ('express');
+const aiController = require("../controllers/ai.controller")
+
 const router = express.Router();
 
-router.get("/get-response", (req,res)=>{
-
-})
+router.post("/get-review", aiController.getReview )
 
 module.exports = router;
